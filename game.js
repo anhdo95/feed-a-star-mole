e0da9775940a67b7a94ea3dd5e5ad6f47db1cdd2
Anh Do <dinhanhdo95@gmail.com>
2020-09-17 23:19:28 +0700

    Update mole's status

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 function Game() {
-  const NEXT_FRAME_INTERVAL = 10000
+  const NEXT_FRAME_INTERVAL = 1000
   const STATUS = {
     SAD: 'SAD',
     HUNGRY: 'HUNGRY',
     LEAVING: 'LEAVING',
     GONE: 'GONE',
   }
+
+  const moles = getMoles()
 
   function getMoles() {
     return Array
@@ -19,21 +21,58 @@ function Game() {
       })
   }
 
-
   function getDefaultInterval() {
     return Date.now() + 1000
+  }
+
+  function updateNextStatus(mole) {
+    switch (mole.status) {
+      case STATUS.SAD:
+        mole.status = STATUS.LEAVING
+        mole.next = getDefaultInterval()
+        mole.node.src = './static/mole-leaving.png'
+        return
+
+      case STATUS.LEAVING:
+        mole.status = STATUS.HUNGRY
+        mole.next = getDefaultInterval()
+        mole.node.src = './static/mole-hungry.png'
+        mole.node.className = 'mole hungry'
+        return
+
+      case STATUS.HUNGRY:
+        mole.status = STATUS.GONE
+        mole.next = getDefaultInterval()
+        mole.node.className = 'mole gone'
+        return  
+
+      case STATUS.GONE:
+        mole.status = STATUS.SAD
+        mole.next = getDefaultInterval()
+        mole.node.src = './static/mole-sad.png'
+        mole.node.className = 'mole'
+        break;
+    }
+  }
+
+  function updateMolesStatus(now) {
+    moles.forEach(function(mole) {
+      if (mole.next < now) {
+        updateNextStatus(mole)
+      }
+    })
   }
 
   this.start = function() {
     let runAgainAt = Date.now()
 
     function nextFrame() {
-      if (runAgainAt < Date.now()) {
-        getMoles().forEach(function(mole) {
-          console.log('mole :>> ', mole);
-        })
+      const now = Date.now()
 
-        runAgainAt = Date.now() + NEXT_FRAME_INTERVAL
+      if (runAgainAt < now) {
+        console.log('now :>> ', now);
+        updateMolesStatus(now)
+        runAgainAt = now + NEXT_FRAME_INTERVAL
       }
 
       requestAnimationFrame(nextFrame)
